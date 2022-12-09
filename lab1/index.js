6bf3abe4f63f663aed1614202327ace55c48f3c5
Oxozon <oxozon.o4@gmail.com>
2022-12-09 19:56:20 +0100

    add initial TCP configuration

--- lab1/index.js
+++ lab1/index.js
@@ -1,24 +1,30 @@
 const express = require('express');
 const app = express();
-const http = require('http');
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
+const net = require('net');
+
+const server = net.createServer((socket) => {
+  socket.write('Welcome new client!\n');
+  console.log('Client connected!');
+
+  socket.on('data', (data) => {
+    console.log(data.toString());
+  });
+
+  socket.on('end', () => {
+    console.log('Client disconnected');
+  });
+
+  socket.on('error', (error) => {
+    const { message } = error;
+    socket.write(`An error occured: ${message}\n`);
+    console.log(`An error occured: ${message}\n`);
+  });
+});
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-  });
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
-  });
-});
-
-server.listen(3000, () => {
-  console.log('listening on *:3000');
+server.listen(8080, () => {
+  console.log('listening on *:8080');
 });
