d2c99ae8bfd891a0679dc5106a95b006437406de
Oxozon <oxozon.o4@gmail.com>
2023-01-06 18:33:05 +0100

    init routing

---
+++ #6 HTTP-server/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'client/index.html'));
+});
+
+app.listen(8080, () => {
+  console.log('Server listening on port 8080');
+});
