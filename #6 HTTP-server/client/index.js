4a25656bd8918d6570d164e861095bb8dee9cd23
Oxozon <oxozon.o4@gmail.com>
2023-01-08 00:52:01 +0100

    finish simple http server

--- #6 HTTP-server/client/index.js
+++ #6 HTTP-server/client/index.js
@@ -1,1 +1,38 @@
 const { pathname } = window.location;
+const formattedPathName = pathname.replace('/', '');
+const tableElement = document.querySelector('.app-table');
+const summaryElement = document.querySelector('.app-summary');
+let summaryValue = 0;
+
+const embedStorageData = async (storageNumber) => {
+  const response = await fetch(`Storage${storageNumber}.txt`);
+  const storageData = await response.text();
+  tableElement.insertAdjacentHTML('beforeend', storageData);
+  setProductsSummaryValue();
+};
+
+const setProductsSummaryValue = () => {
+  if (!summaryElement) {
+    return;
+  }
+  document.querySelectorAll('tr').forEach((element) => {
+    const secondRowValue = element.querySelector('td:nth-child(2)').innerHTML;
+    if (!isNaN(secondRowValue) && !isNaN(parseFloat(secondRowValue))) {
+      summaryValue += Number(secondRowValue);
+    }
+  });
+  summaryElement.innerHTML = `Suma: ${summaryValue}`;
+};
+
+if (formattedPathName) {
+  let isDataEmbeded = false;
+  for (let i = 1; i < 6; i += 1) {
+    if (formattedPathName.includes(i)) {
+      embedStorageData(i);
+      isDataEmbeded = true;
+    }
+  }
+} else if (summaryElement) {
+  summaryElement.innerHTML =
+    'By wybrać magazyn podaj odpowiednią cyfrę (1-5) w url. np http://localhost:8080/1_2';
+}
--- #6 HTTP-server/index.js
+++ #6 HTTP-server/index.js
@@ -2,12 +2,28 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'client/index.html'));
+app.get('*.js', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client/index.js'));
 });
 
-app.get('/index.js', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client/index.js'));
+app.get('/Storage1.txt', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'data/Storage1.txt'));
+});
+app.get('/Storage2.txt', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'data/Storage2.txt'));
+});
+app.get('/Storage3.txt', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'data/Storage3.txt'));
+});
+app.get('/Storage4.txt', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'data/Storage4.txt'));
+});
+app.get('/Storage5.txt', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'data/Storage5.txt'));
+});
+
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'client/index.html'));
 });
 
 app.listen(8080, () => {
