39328f2c6bbec45aa3c8b7b9e2344ca9bb936a56
Oxozon <oxozon.o4@gmail.com>
2023-01-08 00:08:29 +0100

    add client side implementation

--- #6 HTTP-server/index.js
+++ #6 HTTP-server/index.js
@@ -6,6 +6,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'client/index.html'));
 });
 
+app.get('/index.js', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client/index.js'));
+});
+
 app.listen(8080, () => {
   console.log('Server listening on port 8080');
 });
