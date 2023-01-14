0dfc46004195ff65f46b1adad6f6240afe4ab6bc
Oxozon4 <oxozon.o4@gmail.com>
2023-01-14 14:04:35 +0100

    init speed tester

---
+++ #7 Speed-tester/TCP-workers/client.js
@@ -0,0 +1,50 @@
+const net = require("net");
+const prompt = require("prompt");
+
+const SERVER_HOST = "192.168.8.113";
+let SERVER_PORT = 0007;
+
+const getClientMessages = (client) => {
+  prompt.get({ name: "message", message: "Enter message" }, (err, result) => {
+    if (err) {
+      throw err;
+    }
+    if (result.message === "quit") {
+      client.end();
+    }
+    client.write(result.message);
+    console.log(
+      `INFO: Send bytes: ${Buffer.byteLength(result.message, "utf-8")}`
+    );
+    getClientMessages(client);
+  });
+};
+
+const client = net.connect(
+  {
+    port: SERVER_PORT,
+    host: SERVER_HOST,
+  },
+  () => {
+    setTimeout(getClientMessages.bind(this, client), 100);
+  }
+);
+
+client.on("data", (data) => {
+  const stringData = data.toString();
+  console.log(stringData);
+  if (stringData === "Too many connections") {
+    client.end();
+  }
+});
+
+client.on("end", () => {
+  console.log("Disconnected from the server");
+  process.exit();
+});
+
+client.on("error", (error) => {
+  const { message } = error;
+  console.log(`An error occured: ${message}\n`);
+  process.exit();
+});
--- #7 Speed-tester/server.js
+++ #7 Speed-tester/server.js
@@ -1,12 +1,5 @@
-const net = require('net');
+const { Worker } = require('worker_threads');
 const prompt = require('prompt');
-
-const SERVER_HOST = '192.168.8.113';
-const MAX_CONNECTIONS = 3;
-let SERVER_PORT = 0007;
-let ACTIVE_CONNECTIONS = 0;
-
-const clients = [];
 
 prompt.start();
 prompt.get(
@@ -23,80 +16,33 @@ prompt.get(
     if (result.port) {
       SERVER_PORT = Number(result.port);
     }
-    startServer();
+    startServerTCPWorker();
+    startServerUDPWorker();
   }
 );
 
-const startServer = () => {
-  const server = net.createServer((socket) => {
-    ACTIVE_CONNECTIONS += 1;
-    if (ACTIVE_CONNECTIONS > MAX_CONNECTIONS) {
-      socket.write('Too many connections');
-    } else {
-      clients.push(socket);
-      socket.write('Server: Welcome new client!\n');
-      clients.forEach((client) => {
-        client.write(
-          `Server: Active connected clients: ${ACTIVE_CONNECTIONS}\n`
-        );
-      });
-      console.log('Client connected!');
-      console.log(`Client address: ${socket.remoteAddress}`);
-      console.log(`Client port: ${socket.remotePort}`);
-      console.log(`Client IP4/IP6: ${socket.remoteFamily}`);
-    }
+const startServerTCPWorker = () => {
+  const TCPWorker = new Worker('./TCP-workers/server');
 
-    socket.on('data', (data) => {
-      console.log(
-        `Client (PORT:${
-          socket._peername.port
-        }): ${data.toString()} (INFO: Received Bytes: ${data.byteLength})`
-      );
-      clients.forEach((client) => {
-        if (client !== socket) {
-          client.write(
-            `\nClient (IP: ${socket._peername.address}, Port: ${socket._peername.port}): ${data}`
-          );
-        }
-      });
-    });
-
-    socket.on('end', () => {
-      ACTIVE_CONNECTIONS -= 1;
-      console.log('Client disconnected');
-    });
-
-    socket.on('error', (error) => {
-      const { message } = error;
-      socket.write(`Server: An error occured: ${message}\n`);
-      console.log(`An error occured: ${message}\n`);
-      process.exit();
-    });
-
-    socket.on('close', () => {
-      const index = clients.indexOf(socket);
-      if (index !== -1) {
-        clients.splice(index, 1);
-      }
-      ACTIVE_CONNECTIONS -= 1;
-    });
-    console.log(`Server address: ${server.address().address}\n`);
-  });
-
-  server.on('error', (e) => {
-    if (e.message === 'Too many connections') {
-      return;
-    }
-    console.log(`An error occured during creation of the server: ${e}`);
-    console.log(
-      `Are you sure that you have access to IP address: ${SERVER_HOST} ?`
-    );
+  TCPWorker.on('exit', () => {
+    console.log('TCP Worker: Finished all operations!');
     process.exit();
   });
+  TCPWorker.on('error', (msg) => {
+    console.log('TCP Worker: There has been an error with the thread!', msg);
+    process.exit();
+  });
+};
 
-  server.listen({ port: SERVER_PORT, host: SERVER_HOST }, () => {
-    console.log(
-      `Server (ip: ${SERVER_HOST}): Started listening on port: ${SERVER_PORT} ...`
-    );
+const startServerUDPWorker = () => {
+  const UDPWorker = new Worker('./UDP-workers/server.js');
+
+  UDPWorker.on('exit', () => {
+    console.log('UDP Worker: Finished all operations!');
+    process.exit();
+  });
+  UDPWorker.on('error', (msg) => {
+    console.log('UDP Worker: There has been an error with the thread!', msg);
+    process.exit();
   });
 };
